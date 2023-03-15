2900807867ae91a071d2d7882783191310dc7b57
Greg Ivanchuk <gregiv99gmail.com>
2023-03-15 10:00:08 +0200

    added map

--- src/components/Main/Contact/index.js
+++ src/components/Main/Contact/index.js
@@ -2,8 +2,10 @@ import styles from './Contact.module.css';
 function Contact() {
   return (
     <>
-  <h1>Де нас знайти? Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae facere quod aliquid qui sed modi. Tempore, at tenetur. Animi eveniet iste repellendus accusamus veniam. Blanditiis eius eveniet debitis porro.</h1>
-  {/* <iframe height={500} width={1000} src="https://www.google.com.ua/maps/place/Ristorante-Pizzeria+Gianni/@52.5348313,13.3549729,17.57z/data=!4m15!1m8!3m7!1s0x47a84e373f035901:0x42120465b5e3b70!2z0JHQtdGA0LvQuNC9LCDQk9C10YDQvNCw0L3QuNGP!3b1!8m2!3d52.5200066!4d13.404954!16zL20vMDE1NnE!3m5!1s0x47a8519dbcf2a5ad:0x841bedb40d4d1fd5!8m2!3d52.5345412!4d13.353859!16s%2Fg%2F11bzv24tqd?hl=ru" frameborder="0"></iframe> */}
+    <div className={styles.main}>
+  <h1>Де нас знайти? </h1>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d158.6452961929309!2d30.48038625836912!3d50.49091359729203!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1678866279717!5m2!1sru!2sua"  height={450} ></iframe>
+  </div>
     </>
   );
 }
